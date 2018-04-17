19ab82dc42ae7f85d7f943eefab73888d8efdb8a
Ahmad, Bilal (UMKC-Student) <bilal.ahmad@mail.umkc.edu>
2018-04-17 14:24:58 -0500

    gg

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const client2 = Binance({
   apiSecret: '',
 });
 
-var BigArray = [];
+var BigArray = ['kajsldfhaflsadkajsldfhaflsadkajsldfhaflsadkajsldfhaflsadkajsldfhaflsadkajsldfhaflsadkajsldfhaflsadkajsldfhaflsadkajsldfhaflsadkajsldfhaflsadkajsldfhaflsadkajsldfhaflsad'];
 
 tmp = 'bilal';
 
@@ -24,6 +24,23 @@ tmp = 'bilal';
 // }));
 // };
 
+function sum(array) {
+  var num = 0;
+  for (var i = 0, l = array.length; i < l; i++) num += array[i];
+  return num;
+}
+
+function imean(array) {
+  return sum(array) / array.length;
+}
+
+function variance(array) {
+  var mean = imean(array);
+  return imean(array.map(function(num) {
+    return Math.pow(num - mean, 2);
+  }));
+}
+
 function precisionRound(number, precision){
   var factor = Math.pow(10,precision);
   return Math.round(number * factor) / factor;
@@ -36,7 +53,7 @@ function getLargestGradItem(largeArr){
     if(count == 0){
       item = largeArr[coin];
     } else {
-      if(largeArr[coin].gradient > item.gradient){
+      if(largeArr[coin].lowDiff < item.lowDiff){
         item = largeArr[coin];
       }
     }
@@ -64,44 +81,57 @@ async function getPrices(){
     if(BigArray[item.symbol] == undefined){
       BigArray[item.symbol] = {};
       BigArray[item.symbol].history = [];
-      BigArray[item.symbol].history.push(parseFloat(item.lastPrice));
-      BigArray[item.symbol].gradient = 0;
+      BigArray[item.symbol].history.push(parseFloat(item.priceChangePercent));
+      // BigArray[item.symbol].gradient = 0;
+      // BigArray[item.symbol].trend = 0;
       BigArray[item.symbol].symbol = item.symbol;
     } else {
 
-      if(BigArray[item.symbol].history.length == 5){
+      if(BigArray[item.symbol].history.length == 10){
         BigArray[item.symbol].history.splice(0,1);
-        BigArray[item.symbol].gradient = computeFiveGradient(BigArray[item.symbol]);
+        // BigArray[item.symbol].gradient = computeFiveGradient(BigArray[item.symbol]);
         // BigArray[item.symbol].gradient = precisionRound((BigArray[item.symbol].gradient + (parseFloat(item.lastPrice) - BigArray[item.symbol].history[BigArray[item.symbol].history.length - 1]))/2,8);
-        BigArray[item.symbol].history.push(parseFloat(item.lastPrice));
+        BigArray[item.symbol].history.push(parseFloat(item.priceChangePercent));
+        //BigArray[item.symbol].trend = BigArray[item.symbol].history[BigArray[item.symbol].history.length - 1] - BigArray[item.symbol].history[0];
+        //BigArray[item.symbol].lowDiff = variance(BigArray[item.symbol].history)  * (item.lastPrice - item.lowPrice);
       } else {
-        BigArray[item.symbol].gradient = precisionRound((BigArray[item.symbol].gradient + (parseFloat(item.lastPrice) - BigArray[item.symbol].history[BigArray[item.symbol].history.length - 1]))/2,8);
-        BigArray[item.symbol].history.push(parseFloat(item.lastPrice));
+        // BigArray[item.symbol].gradient = precisionRound((BigArray[item.symbol].gradient + (parseFloat(item.lastPrice) - BigArray[item.symbol].history[BigArray[item.symbol].history.length - 1]))/2,8);
+        BigArray[item.symbol].history.push(parseFloat(item.priceChangePercent));
       }
     }
   }
   }
-  var pitem = getLargestGradItem(BigArray);
-  pitem = pitem.gradient;
-  // console.log(pitem);
+  // var pitem = getLargestGradItem(BigArray);
+  // pitem = pitem.gradient;
+  // console.log(BigArray);
+  return BigArray;
 };
 
-getPrices();
+async function getBGR(){
+   return await BigArray
+};
 
-setInterval(getPrices,10000);
-// test();
+async function getAccountInfo(){
+  var assets = [];
+  var coins = await client2.accountInfo();
+  for(coin in coins.balances){
+    if(parseFloat(coins.balances[coin].free) != 0)
+      assets.push(coins.balances[coin]);
+  }
+}
 
-// var express = require('express');
-// var app = express();
+getAccountInfo();
+var express = require('express');
+  var app = express();
 
-// app.get('/data',function(req,res){
-	// promise = getPrices();
-	// console.log(tmp);
-	
-	// promise.then(function(result) {
-        // // use the result...
-        // res.send(result);
-    // });
-// });
+  app.get('/data',function(req,res){
+    getPrices().then(function(result){
+      console.log(result);
+      res.send(typeof result);
+    });
+  });
 
-// app.listen(80);
+  // setInterval(getPrices,10000);
+  app.listen(80);
+//test();
+
